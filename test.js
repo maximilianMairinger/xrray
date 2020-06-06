dcd9d3b51494c81d8e4708cef0e62925383b6e15
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-06-06 13:23:02 +0200

    test

--- test.js
+++ test.js
@@ -4,26 +4,5 @@ require('./xrray')(Array);
 
 
 
-let a = {
-  w: "qwe1",
-  q: (a) => a+1,
-  e: "asd"
-}
-
-let b = {
-  w: "qwe",
-  q: (a) => a+2,
-  r: "asd"
-}
-
-let e = [a.q, b.q]
-
-
-console.log(e.call(3))
-
-
-let x = [2,3]
-
-x.replace(e => {return {a: e}})
-
-console.log(x)
+let a = [undefined, null, 2, null, 2, 2, null, null, null, 2, null]
+console.log(a.clean())
